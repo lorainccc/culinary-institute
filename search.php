c87acff3b0954ea1557383bbb4d5d0d3f022c580
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-03-22 09:53:12 -0400

    Updated Search Template based upon current LCCC Site

--- search.php
+++ search.php
@@ -1,43 +1,29 @@
-<?php 
+<?php
 
-/* 
-This template is for search results. 
-*/
+get_header();
+?>
+<div class="row">
+	<div class="small-12 columns">
+		<h1><?php printf( esc_html__( 'Search Results for: %s', 'lorainccc' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+	</div>
+</div>
+<div class="row">
+	<div class="small-12 columns">
+<?php	
 
-get_header(); 
+ while ( have_posts() ) : the_post();
+
+		switch_to_blog($post->blog_id);
+	 
+		get_template_part( 'template-parts/content', 'search' );
+  
+		restore_current_blog();
+		
+ endwhile; // end of the loop.
+?>
+	</div>
+</div>
+	<?php
+get_footer();
 
 ?>
-			
-<div id="search-content">
-	
-	<?php 
-	
-	if (have_posts()) :  
-	
-		get_template_part( 'template-parts/content', 'search-banner' );
-		
-	?>
-	
-	<div class="row">
-	
-		<main id="main" role="main" class="small-12 columns">
-			
-			<?php 
-			
-			while (have_posts()) : the_post();
-			
-				get_template_part( 'template-parts/loop', 'search');
-				
-			endwhile;
-				
-			?>
-
-		</main>
-		
-	</div>
-	
-	<?php  endif; ?>
-	
-</div>
-
-<?php get_footer(); ?>
